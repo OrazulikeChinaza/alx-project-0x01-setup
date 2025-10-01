39c7af4594eb2ba07a9907ba6e980d146ffe3c87
Orazulike Chinaza <orazulikechinaza@gmail.com>
2025-10-01 17:47:57 +0100

    final fix

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,37 +2,36 @@ import React, { useState } from "react";
 import Button from "@/components/common/Button";
 import UserModal from "@/components/common/UserModal";
 import { UserData } from "@/interfaces";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
+interface UsersPageProps {
+  users: UserProps[];
+}
 
-const UsersPage: React.FC = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [users, setUsers] = useState<UserData[]>([]);
-
-  const handleSaveUser = (newUser: UserData) => {
-    setUsers((prev) => [...prev, newUser]);
-  };
-
+const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <main className="p-6">
-      <h1 className="text-3xl font-semibold mb-4">Users</h1>
-
-      <Button label="Add User" onClick={() => setIsModalOpen(true)} />
-
-      {/* Render Modal */}
-      <UserModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onSave={handleSaveUser}
-      />
-
-      <ul className="mt-6">
-        {users.map((user) => (
-          <li key={user.id} className="p-2 border rounded mb-2">
-            <strong>{user.name}</strong> ({user.username}) - {user.email}
-          </li>
-        ))}
-      </ul>
+      <h1 className="text-3xl font-bold mb-6">Users</h1>
+      {users.map((user) => (
+        <UserCard
+          key={user.id}
+          id={user.id}
+          name={user.name}
+          email={user.email}
+          address={user.address}
+        />
+      ))}
     </main>
   );
 };
 
-export default UsersPage;
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: UserProps[] = await res.json();
+
+  return {
+    props: { users },
+  };
+}
+
+export default Users;
